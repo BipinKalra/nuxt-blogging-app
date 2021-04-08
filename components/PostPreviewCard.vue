ce003ee4a74d4c1fce0baef0e6469256a0949739
BipinKalra <bipinkalra@gmail.com>
2021-04-08 14:46:55 +0530

    separate component for post preview card

---
+++ components/PostPreviewCard.vue
@@ -0,0 +1,52 @@
+<template>
+  <nuxt-link :to="`/posts/${1}`" class="post-preview">
+    <div>
+      <div
+        class="post-thumbnail"
+        style="background-image: url('https://res.cloudinary.com/people-matters/image/upload/q_auto,f_auto/v1578710070/1578710068.jpg')"
+      ></div>
+      <div class="post-content">
+        <div class="heading-1 bold">Post Title</div>
+        <div>Preview Text</div>
+      </div>
+    </div>
+  </nuxt-link>
+</template>
+
+<style scoped>
+.post-preview {
+  border: 1px solid #ccc;
+  box-shadow: 0 2px 2px #ccc;
+  background-color: white;
+  width: 90%;
+}
+
+a {
+  text-decoration: none;
+  color: black;
+}
+
+@media (min-width: 850px) {
+  .post-preview {
+    width: 400px;
+    margin: 10px;
+  }
+}
+
+.post-thumbnail {
+  width: 100%;
+  height: 200px;
+  background-position: center;
+  background-size: cover;
+}
+
+.post-content {
+  padding: 10px;
+  text-align: center;
+}
+
+a:hover .post-content,
+a:active .post-content {
+  background-color: #ccc;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -4,36 +4,19 @@
       <div class="heading-1 bold page-title">Get the latest tech news!</div>
     </div>
     <div class="featured-posts">
-      <nuxt-link :to="`/posts/${1}`" class="post-preview">
-        <div>
-          <div
-            class="post-thumbnail"
-            style="background-image: url('https://res.cloudinary.com/people-matters/image/upload/q_auto,f_auto/v1578710070/1578710068.jpg')"
-          ></div>
-          <div class="post-content">
-            <div class="heading-1 bold">Post Title</div>
-            <div>Preview Text</div>
-          </div>
-        </div>
-      </nuxt-link>
-      <nuxt-link :to="`/posts/${1}`" class="post-preview">
-        <div>
-          <div
-            class="post-thumbnail"
-            style="background-image: url('https://res.cloudinary.com/people-matters/image/upload/q_auto,f_auto/v1578710070/1578710068.jpg')"
-          ></div>
-          <div class="post-content">
-            <div class="heading-1 bold">Post Title</div>
-            <div>Preview Text</div>
-          </div>
-        </div>
-      </nuxt-link>
+      <PostPreviewCard />
+      <PostPreviewCard />
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+import PostPreviewCard from "@/components/PostPreviewCard.vue";
+export default {
+  components: {
+    PostPreviewCard
+  }
+};
 </script>
 
 <style scoped>
@@ -74,41 +57,5 @@ export default {};
   flex-wrap: wrap;
   align-items: center;
   justify-content: center;
-}
-
-.post-preview {
-  border: 1px solid #ccc;
-  box-shadow: 0 2px 2px #ccc;
-  background-color: white;
-  width: 90%;
-}
-
-a {
-  text-decoration: none;
-  color: black;
-}
-
-@media (min-width: 850px) {
-  .post-preview {
-    width: 400px;
-    margin: 10px;
-  }
-}
-
-.post-thumbnail {
-  width: 100%;
-  height: 200px;
-  background-position: center;
-  background-size: cover;
-}
-
-.post-content {
-  padding: 10px;
-  text-align: center;
-}
-
-a:hover .post-content,
-a:active .post-content {
-  background-color: #ccc;
 }
 </style>

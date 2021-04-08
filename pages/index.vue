a6b01745793e3102acae4679637627f8bcb9a191
BipinKalra <bipinkalra@gmail.com>
2021-04-08 14:43:38 +0530

    post styling

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,29 @@
 <template>
   <div class="homepage">
     <div class="intro">
-      <div class="heading-1 bold">Get the latest tech news!</div>
+      <div class="heading-1 bold page-title">Get the latest tech news!</div>
     </div>
     <div class="featured-posts">
-      <nuxt-link :to="`/posts/${1}`">
-        <div class="post-preview">
-          <div class="post-thumbnail"></div>
+      <nuxt-link :to="`/posts/${1}`" class="post-preview">
+        <div>
+          <div
+            class="post-thumbnail"
+            style="background-image: url('https://res.cloudinary.com/people-matters/image/upload/q_auto,f_auto/v1578710070/1578710068.jpg')"
+          ></div>
           <div class="post-content">
-            <div class="heading-1 bold post-title"></div>
+            <div class="heading-1 bold">Post Title</div>
+            <div>Preview Text</div>
+          </div>
+        </div>
+      </nuxt-link>
+      <nuxt-link :to="`/posts/${1}`" class="post-preview">
+        <div>
+          <div
+            class="post-thumbnail"
+            style="background-image: url('https://res.cloudinary.com/people-matters/image/upload/q_auto,f_auto/v1578710070/1578710068.jpg')"
+          ></div>
+          <div class="post-content">
+            <div class="heading-1 bold">Post Title</div>
             <div>Preview Text</div>
           </div>
         </div>
@@ -16,3 +31,84 @@
     </div>
   </div>
 </template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.intro {
+  height: 300px;
+  position: relative;
+  padding: 30px;
+  box-sizing: border-box;
+  background-position: center;
+  background-size: cover;
+}
+
+.intro .page-title {
+  position: absolute;
+  top: 10%;
+  left: 5%;
+  width: 90%;
+  font-size: 1.5rem;
+  color: black;
+  background-color: rgb(211, 211, 211);
+  padding: 10px;
+  border-radius: 10px;
+  box-shadow: 3px 3px 3px black;
+  box-sizing: border-box;
+  border: 1px solid black;
+}
+
+@media (min-width: 768px) {
+  .intro .page-title {
+    font-size: 2rem;
+  }
+}
+
+.featured-posts {
+  display: flex;
+  padding: 20px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
+
+.post-preview {
+  border: 1px solid #ccc;
+  box-shadow: 0 2px 2px #ccc;
+  background-color: white;
+  width: 90%;
+}
+
+a {
+  text-decoration: none;
+  color: black;
+}
+
+@media (min-width: 850px) {
+  .post-preview {
+    width: 400px;
+    margin: 10px;
+  }
+}
+
+.post-thumbnail {
+  width: 100%;
+  height: 200px;
+  background-position: center;
+  background-size: cover;
+}
+
+.post-content {
+  padding: 10px;
+  text-align: center;
+}
+
+a:hover .post-content,
+a:active .post-content {
+  background-color: #ccc;
+}
+</style>

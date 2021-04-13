6bb3f1416c66ad18bc19a667e330a0678b7723d7
BipinKalra <bipinkalra@gmail.com>
2021-04-13 18:13:12 +0530

    emulating async data for posts

--- pages/index.vue
+++ pages/index.vue
@@ -14,30 +14,62 @@ export default {
   components: {
     PostList
   },
-  data() {
-    return {
-      posts: [
-        {
-          id: 1,
-          thumbnail:
-            "https://res.cloudinary.com/people-matters/image/upload/q_auto,f_auto/v1578710070/1578710068.jpg",
-          title: "Fantastic Post",
-          previewText: "This is the preview for a fantastic post!",
-          content:
-            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
-        },
-        {
-          id: 2,
-          thumbnail:
-            "https://res.cloudinary.com/people-matters/image/upload/q_auto,f_auto/v1578710070/1578710068.jpg",
-          title: "Fantastic Post 2",
-          previewText: "This is the preview for a fantastic post!",
-          content:
-            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
-        }
-      ]
-    };
+  asyncData(context, callback) {
+    setTimeout(() => {
+      callback(null, {
+        posts: [
+          {
+            id: 1,
+            thumbnail:
+              "https://res.cloudinary.com/people-matters/image/upload/q_auto,f_auto/v1578710070/1578710068.jpg",
+            title: "Fantastic Post",
+            previewText: "This is the preview for a fantastic post!",
+            content:
+              "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+          },
+          {
+            id: 2,
+            thumbnail:
+              "https://res.cloudinary.com/people-matters/image/upload/q_auto,f_auto/v1578710070/1578710068.jpg",
+            title: "Fantastic Post 2",
+            previewText: "This is the preview for a fantastic post!",
+            content:
+              "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+          }
+        ]
+      });
+    }, 1500);
   }
+  // data() {
+  //   return {
+  //     posts: []
+  //   };
+  // },
+  // created() {
+  //   // This setTimeout would be replaced by an actual async api call in real world scenario
+  //   setTimeout(() => {
+  //     this.posts = [
+  //       {
+  //         id: 1,
+  //         thumbnail:
+  //           "https://res.cloudinary.com/people-matters/image/upload/q_auto,f_auto/v1578710070/1578710068.jpg",
+  //         title: "Fantastic Post",
+  //         previewText: "This is the preview for a fantastic post!",
+  //         content:
+  //           "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+  //       },
+  //       {
+  //         id: 2,
+  //         thumbnail:
+  //           "https://res.cloudinary.com/people-matters/image/upload/q_auto,f_auto/v1578710070/1578710068.jpg",
+  //         title: "Fantastic Post 2",
+  //         previewText: "This is the preview for a fantastic post!",
+  //         content:
+  //           "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+  //       }
+  //     ];
+  //   }, 1500);
+  // }
 };
 </script>
 
